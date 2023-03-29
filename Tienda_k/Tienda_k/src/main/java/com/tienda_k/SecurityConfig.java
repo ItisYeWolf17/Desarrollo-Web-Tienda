795da43830d811637ecddaf00618c7f08a922492
ItisYeWolf17 <105758299+ItisYeWolf17@users.noreply.github.com>
2023-03-28 20:51:29 -0600

    Semana 11
    Se hace security

--- Tienda_k/Tienda_k/src/main/java/com/tienda_k/SecurityConfig.java
+++ Tienda_k/Tienda_k/src/main/java/com/tienda_k/SecurityConfig.java
@@ -6,10 +6,12 @@ package com.tienda_k;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  *
@@ -35,7 +37,44 @@ public class SecurityConfig {
                 .password("{noop}789")
                 .roles("USER")
                 .build();
-        
-        return new InMemoryUserDetailsManager(usuario,vendedor,admin);
+
+        return new InMemoryUserDetailsManager(usuario, vendedor, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/",
+                        "/index",
+                        "/errores/**",
+                        "/webjars/**").permitAll()
+                .requestMatchers(
+                        "/articulo/nuevo",
+                        "/articulo/guardar",
+                        "/articulo/modificar/**",
+                        "/articulo/eliminar/**",
+                        "/categoria/nuevo",
+                        "/categoria/guardar",
+                        "/categoria/modificar/**",
+                        "/categoria/eliminar/**",
+                        "/cliente/nuevo",
+                        "/cliente/guardar",
+                        "/cliente/modificar/**",
+                        "/cliente/eliminar/**"
+                ).hasRole("ADMIN")
+                .requestMatchers(
+                        "/articulo/listado",
+                        "/categoria/listado",
+                        "/cliente/listado"
+                ).hasAnyRole("ADMIN", "VENDEDOR")
+                )
+                .formLogin((form) -> form
+                .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll())
+                .exceptionHandling()
+                .accessDeniedPage("/errores/403");
+        return http.build();
     }
 }
